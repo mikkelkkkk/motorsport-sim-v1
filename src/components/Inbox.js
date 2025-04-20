9dd1fa8971845a7c71ec674531383588499a66e2
mikkelkkkk <stabenfeldtmikkel@gmail.com>
2025-04-20 23:55:26 +0200

    Delete src/components directory

--- src/components/Inbox.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const Inbox = () => <div><h2>Inbox</h2><p>Team offers, news, and race updates will appear here.</p></div>;
-export default Inbox;
--- src/components/Standings.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const Standings = () => <div><h2>Championship Standings</h2><ol><li>You - 86pts</li><li>Rival - 72pts</li></ol></div>;
-export default Standings;
